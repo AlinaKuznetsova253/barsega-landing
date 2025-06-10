876749e88bd9e59f0c4bd8bf500070d5373b6449
Alina <kuznetsova646@gmail.com>
2025-06-10 15:02:09 +0500

    init

---
+++ src/js/swiper.js
@@ -0,0 +1,17 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
+});
