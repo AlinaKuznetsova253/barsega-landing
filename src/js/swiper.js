d2acf1d01c47d0bb5dcb9a8a358a6a615bf339bd
Alina <kuznetsova646@gmail.com>
2025-06-18 17:36:21 +0500

    adaptive screen 1024px

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -5,23 +5,33 @@ import "swiper/swiper-bundle.css";
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     loop: true,
-    slidesPerView: 4.5,
+    slidesPerView: 3.5,
     spaceBetween: 0,
     navigation: {
       nextEl: ".swiper-button-right",
       prevEl: ".swiper-button-left",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      1025: {
+        slidesPerView: 4.5,
+      },
+    },
   });
 
   const swiperSecond = new Swiper(".swiper-second", {
     loop: true,
-    slidesPerView: 3.5,
+    slidesPerView: 2.5,
     spaceBetween: 0,
     navigation: {
       nextEl: ".swiper-button-right-about",
       prevEl: ".swiper-button-left-about",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      1025: {
+        slidesPerView: 3.5,
+      },
+    },
   });
 });
